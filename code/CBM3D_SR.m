c2f9ccb0b8eb142c83196c97d49686812532c8f6
Joe Driscoll <32208193+jwd0023@users.noreply.github.com>
2019-04-06 20:07:10 -0500

    Update CBM3D_SR.m

--- code/CBM3D_SR.m
+++ code/CBM3D_SR.m
@@ -9,22 +9,54 @@
 % - Main function that implements the super-resolution algorithm.
 %
 % - Arguments:
+%
 %     - required
 %         - input_image:     original (possibly noisy) input image
 %         - scale:           scaling/upsampling ratio
 %         - sigma:           sigma of additive white gaussian noise
-%     - optional
-%         - max_iterations:  limit number of iterations - default 40
-%         - show_iterations: display updated output estimate at each step
 %
-function upsampled_image = SR_CBM3D(input_image, scale, sigma, max_iterations, show_iterations)
+%     - optional
+%         - max_iterations:  limit number of iterations                   - default 40
+%         - show_iterations: display updated output estimate at each step - default 1
+%
+function upsampled_image = SR_CBM3D(input_image, scale, sigma, beta, max_iterations, show_iterations)
 
-    % Denoise the input image using BM3D before upsampling (may not be needed).
-    [unused, filtered_input] = BM3D(1, input_image, sigma);
+    % Denoise the input image using CBM3D before upsampling (may not be needed).
+    [unused, filtered_input] = CBM3D(1, input_image, sigma);
 
     % Make initial estimate using bicubic interpolation.
-    upsampled_image = imresize(input_image, scale, 'bicubic');
+    upsampled_image = imresize(filtered_input, scale, 'bicubic');
     
-    % Set default parameters 
+    % Set default values for optional parameters if they are not input.
+    if (exist('beta') ~= 1)
+        beta = 1.75;
+    end
+    
+    if (exist('max_iterations') ~= 1)
+        max_iterations = 40;
+    end
+    
+    if (exist('show_iterations') ~= 1)
+        show_iterations = 1
+    end
+    
+    % Show the 0-th iterate if needed.
+    if (show_iterations == 1)
+        figure; imagesc(upsampled_image); axis off;
+        title('Iterate 0');
+    end
+    
+    % Main loop that performs the main portion of the algorithm.
+    for i = 1:max_iterations
+    
+        % Stage 1 - Apply CMB3D to current iterate.
+        [unused,upsampled_image] = CBM3D(1, upsampled_image, sigma);
+        
+        % Stage 2 - Next iterate is current iterate, plus the weighted, upsampled 
+        %           difference between the original image and the downscaled version
+        %           of the current iterate.
+        
+    
+    end
     
 end
