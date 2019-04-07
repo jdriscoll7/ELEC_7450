c3d15cf3cf1b2bcb57f789a61f921c7e90e12bd3
Joe Driscoll <32208193+jwd0023@users.noreply.github.com>
2019-04-06 19:16:42 -0500

    Update CBM3D_SR.m

--- code/CBM3D_SR.m
+++ code/CBM3D_SR.m
@@ -1,1 +1,16 @@
+% File: SR_CBM3D.m 
+%
+% - Implements the super-resolution algorithm based on (C)BM3D, as in the paper
+%   https://github.com/jwd0023/ELEC_7450/blob/master/papers/egiazarian_super_resolution_with_bm3d.pdf.
 
+
+% Function: SR_CMB3D
+%
+% - Main function that implements the super-resolution algorithm.
+%
+% - Arguments:
+%     - input_image: original (possibly noisy) input image
+%     - scale:       mxn scaling/upsampling ratio
+%     - sigma:       sigma of additive white gaussian noise
+%
+function upsampled_image = SR_CBM3D(input_image, scale, sigma)
